bcd67e4fa61336bb224a075b357bb6f6b534aa3c
sajitheranda05 <70995241+sajitheranda05@users.noreply.github.com>
2021-08-13 18:17:15 +0530

    home page and admin login added

---
+++ app/src/main/java/com/smartFitness/home/MainAdminLogin.java
@@ -0,0 +1,14 @@
+package com.smartFitness.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainAdminLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_admin_login);
+    }
+}
