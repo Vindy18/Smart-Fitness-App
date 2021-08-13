c9c630ab40d65ab6496f4b6e9eb5699f16ec9ea8
Harshaprabath <prabhathhemasir@gmail.com>
2021-08-13 14:18:12 +0530

    custermer view

---
+++ app/src/main/java/com/smartFitness/home/Admin_view_Activity.java
@@ -0,0 +1,14 @@
+package com.smartFitness.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Admin_view_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_view);
+    }
+}
