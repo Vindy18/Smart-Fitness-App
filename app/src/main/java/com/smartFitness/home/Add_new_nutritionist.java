9ae992ab7c5287f5d146066de922ecaa09aa3def
sasanka <it20037956@my.sliit.lk>
2021-08-14 22:55:06 +0530

    Nutritionist pages created

---
+++ app/src/main/java/com/smartFitness/home/Add_new_nutritionist.java
@@ -0,0 +1,14 @@
+package com.smartFitness.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_new_nutritionist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_nutritionist);
+    }
+}
---
+++ app/src/main/java/com/smartFitness/home/Nutritionist_profile_view.java
@@ -0,0 +1,14 @@
+package com.smartFitness.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Nutritionist_profile_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nutritionist_profile_view);
+    }
+}
