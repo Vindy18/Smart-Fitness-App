6a1aa09b8bb266804ac4018bf2af497ebcb2ef39
sasanka <it20037956@my.sliit.lk>
2021-08-14 06:36:54 +0530

    Nutritionist user view created

---
+++ app/src/main/java/com/smartFitness/home/NutritionistSearchingView.java
@@ -0,0 +1,14 @@
+package com.smartFitness.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NutritionistSearchingView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nutritionist_searching_view);
+    }
+}
