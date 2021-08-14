c8986b77d6ea582925e0d95364e2949cb38737c1
sajitheranda05 <70995241+sajitheranda05@users.noreply.github.com>
2021-08-14 15:41:48 +0530

    set of views added

---
+++ app/src/main/java/com/smartFitness/home/customer_view_cardio_ins_list.java
@@ -0,0 +1,14 @@
+package com.smartFitness.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class customer_view_cardio_ins_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_view_cardio_ins_list);
+    }
+}
