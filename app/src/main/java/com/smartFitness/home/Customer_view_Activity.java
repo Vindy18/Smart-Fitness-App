045e4048ec5559e4681b916951d62c606d16178f
Harshaprabath <prabhathhemasiri@gmail.com>
2021-09-09 15:46:54 +0530

    start intent - loginAsAnAdmin intent and theam color chenge bmi intent done

--- app/src/main/java/com/smartFitness/home/Customer_view_Activity.java
+++ app/src/main/java/com/smartFitness/home/Customer_view_Activity.java
@@ -2,10 +2,16 @@ package com.smartFitness.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Customer_view_Activity extends AppCompatActivity {
+
+    Button btn_bmiCal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,16 @@ public class Customer_view_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_customer_view);
 
         Intent mainIntent = getIntent();
+
+        btn_bmiCal = findViewById(R.id.btn_bmiCal);
+
+    }
+
+    public void BMIonClick(View view) {
+        Intent intent = new Intent(Customer_view_Activity.this,Customer_view_BMI_calculeter.class);
+        startActivity(intent);
+
+        Context context = getApplicationContext();
+        Toast.makeText(context,"BMI Calculator is Loading ",Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/smartFitness/home/Customer_view_BMI_calculeter.java
+++ app/src/main/java/com/smartFitness/home/Customer_view_BMI_calculeter.java
@@ -2,6 +2,7 @@ package com.smartFitness.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Customer_view_BMI_calculeter extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class Customer_view_BMI_calculeter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_view_bmi_calculeter);
+
+        Intent mainIntent = getIntent();
     }
 }
