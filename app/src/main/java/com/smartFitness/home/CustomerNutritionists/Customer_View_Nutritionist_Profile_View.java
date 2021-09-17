585596b91124505d773c29f888f3669506e16bd3
sasanka <it20037956@my.sliit.lk>
2021-09-17 17:10:10 +0530

    master, DB helper data bases created xml and java files renamed

---
+++ app/src/main/java/com/smartFitness/home/CustomerNutritionists/Customer_View_Nutritionist_Profile_View.java
@@ -0,0 +1,16 @@
+package com.smartFitness.home.CustomerNutritionists;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.smartFitness.home.R;
+
+public class Customer_View_Nutritionist_Profile_View extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_view_nutritionist_profile_view);
+    }
+}
