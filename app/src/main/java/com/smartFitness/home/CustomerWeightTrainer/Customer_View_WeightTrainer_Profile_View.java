5db7312952f5278c0b31d28b58e56ab494172768
Vindy18 <devindip69@gmail.com>
2021-10-03 18:33:47 +0530

    Weight Trainer changed

---
+++ app/src/main/java/com/smartFitness/home/CustomerWeightTrainer/Customer_View_WeightTrainer_Profile_View.java
@@ -0,0 +1,16 @@
+package com.smartFitness.home.CustomerWeightTrainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.smartFitness.home.R;
+
+public class Customer_View_WeightTrainer_Profile_View extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_view_weight_trainer_profile_view);
+    }
+}
